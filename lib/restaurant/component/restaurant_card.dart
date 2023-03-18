fd9e41a4f29c86895464245a395af9fa74ced38a
lsuinl <su96in43@naver.com>
2023-03-18 18:27:35 +0900

    음식점 카드 UI제작

---
+++ lib/restaurant/component/restaurant_card.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant/common/const/colors.dart';
+
+class RestaurantCard extends StatelessWidget {
+  //이미지
+  final Widget image;
+
+  //레스토랑이름
+  final String name;
+
+  //레스토랑 태그
+  final List<String> tags;
+
+  //평점 갯수
+  final int ratingCount;
+
+  //배송시간
+  final int deliveryTime;
+
+  //배송 비용
+  final int deliveryFee;
+
+  //평균 평점
+  final double rating;
+
+  const RestaurantCard({
+    required this.image,
+    required this.name,
+    required this.tags,
+    required this.ratingCount,
+    required this.deliveryTime,
+    required this.deliveryFee,
+    required this.rating,
+    Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ClipRRect( //이미지 깎아서 만들어요
+          borderRadius: BorderRadius.circular(12.0),
+          child: image,
+        ),
+        const SizedBox(height: 16.0),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(name,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+            const SizedBox(height: 8),
+            Text(tags.join(' · '),
+              style: TextStyle(color: BODY_TEXT_COLOR, fontSize: 14),)
+            //join을 통해 리스트를 분류하고 그 사잇값을 넣어줌
+          ],
+        ),
+        const SizedBox(height: 8),
+        Row(
+          children: [
+            _IconText(icon: Icons.star, label: rating.toString()),
+            renderDot(),
+            _IconText(icon: Icons.receipt, label: ratingCount.toString()),
+            renderDot(),
+            _IconText(icon: Icons.timelapse_outlined, label: '$deliveryTime분'),
+            renderDot(),
+            _IconText(icon: Icons.monetization_on,
+                label: '${deliveryFee == 0 ? "무료" : deliveryFee.toString()}'),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget renderDot() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4),
+      child: Text(
+        ' · ',
+        style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+      ),
+    );
+  }
+}
+
+class _IconText extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const _IconText({
+    required this.icon,
+    required this.label,
+    Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(icon,color:PRIMARY_COLOR,size: 14),
+        const SizedBox(width: 7),
+        Text(label,style: TextStyle(fontSize: 12,fontWeight: FontWeight.w500),)
+      ],
+    );
+  }
+}
