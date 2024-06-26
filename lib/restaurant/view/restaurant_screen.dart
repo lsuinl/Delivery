40fb6805486a3ddfa9b5c8d827d4d1bcbb268a2f
SUIN <87413903+lsuinl@users.noreply.github.com>
2024-06-26 23:25:56 +0900

    완성된 pagination로직 적용실행

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -5,26 +5,58 @@ import 'package:restaurant/restaurant/component/restaurant_card.dart';
 import 'package:restaurant/restaurant/provider/restaurant_provider.dart';
 import 'package:restaurant/restaurant/view/restaurant_detail_screen.dart';
 
-class RestaurantScreen extends ConsumerWidget {
-  const RestaurantScreen({Key? key}) : super(key: key);
+class RestaurantScreen extends ConsumerStatefulWidget {
+  const RestaurantScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initstate(){
+    super.initState();
+
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener(){
+    //현재 위치가 최대 길이보다 조금 덜 되는 위치까지 왔다면
+    //새로운 데이터의 추가 요청
+    if(controller.offset>controller.position.maxScrollExtent-300)
+      ref.read(restaurantProvider.notifier).paginate(
+        fetchMore: true
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     //어떤 순간에서든 restaurantProvider이 생성됨. (필요가없어지면 알아서 삭제)
     final data = ref.watch(restaurantProvider);
 
-    //로딩상태
+    //최로 로딩상태
     if(data is CursorPaginationLoading){
       return Center(
         child:CircularProgressIndicator(),
       );
     }
-    //데이터가 들어왔다고 가정하고 데이터를 불러옵니다.
+
+    //에러 상태
+    if(data is CursorPaginationError){
+      return Center(
+        child: Text(data.message)
+      );
+    }
+
+    //CursorPagination, FetchingMore, Refetching ...
     final cp = data as CursorPagination;
 
     return Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: ListView.separated(
+            controller: controller,
                 itemCount: cp.data.length,
                 itemBuilder: (_, index) {
                   final pItem = cp.data[index];
