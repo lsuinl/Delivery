a79c13ea8050343a09d7bdfce73014c9737d65f4
SUIN <87413903+lsuinl@users.noreply.github.com>
2024-07-07 02:02:47 +0900

    ratingcard  Image, body작업

--- lib/rating/component/rating_card.dart
+++ lib/rating/component/rating_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant/common/const/colors.dart';
+import 'package:collection/collection.dart'; //map에서 인덱스도 보는 방법
 
 class RatingCard extends StatelessWidget {
   final ImageProvider avatarImage; //네트워크이미지, 에셋이미지,circleavatar
@@ -19,7 +20,18 @@ class RatingCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [_Header(), _Body(), _Images()],
+      children: [
+        _Header(avaterImage: avatarImage, rating: rating, email: email),
+        const SizedBox(height: 8.0),
+        _Body(content: content),
+       if(images.length>0)
+        SizedBox(
+          height: 100,
+          child:
+        _Images(
+          images: images,
+        ))
+      ],
     );
   }
 }
@@ -54,8 +66,7 @@ class _Header extends StatelessWidget {
         ...List.generate(
             5,
             (index) => Icon(
-              index<rating?Icons.star:
-              Icons.star_border_outlined,
+                  index < rating ? Icons.star : Icons.star_border_outlined,
                   color: PRIMARY_COLOR,
                 ))
       ],
@@ -64,19 +75,42 @@ class _Header extends StatelessWidget {
 }
 
 class _Body extends StatelessWidget {
-  const _Body({super.key});
+  final String content;
+
+  const _Body({required this.content, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(children: [
+      //텍스트가 넘치면 다음줄로 넘어가게해준다 이 Flexible가
+      Flexible(
+        child: Text(
+          content,
+          style: TextStyle(color: BODY_TEXT_COLOR, fontSize: 14),
+        ),
+      )
+    ]);
   }
 }
 
 class _Images extends StatelessWidget {
-  const _Images({super.key});
+  final List<Image> images;
+
+  const _Images({required this.images, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView(
+        scrollDirection: Axis.horizontal,
+        children: images
+            .mapIndexed((index, e) => Padding(
+                //마지막 인덱스의 사진에는 패딩을 넣지 않음
+                padding:
+                    EdgeInsets.only(right: index == images.length - 1 ? 0 : 16),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: e,
+                )))
+            .toList());
   }
 }
