6cabdd0d114bcf42a65a7ba0141a83a9d8d0c72f
SUIN <87413903+lsuinl@users.noreply.github.com>
2024-07-09 09:59:49 +0900

    RatingModel 작업하기

---
+++ lib/user/model/user_model.dart
@@ -0,0 +1,23 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:restaurant/common/utils/data_utils.dart';
+
+part 'user_model.g.dart';
+
+@JsonSerializable()
+class UserModel {
+final String id;
+final String username;
+@JsonKey(
+  fromJson: DataUtils.pathToUrl
+)
+final String imageUrl;
+
+UserModel({
+required this.id,
+required this.username,
+required this.imageUrl,
+});
+factory UserModel.fromJson(Map<String,dynamic> json)
+=> _$UserModelFromJson(json);
+}
+//펍빌드워치어쩌고하기~
